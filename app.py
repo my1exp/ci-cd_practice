8cdb8e3136413921c33741570351c33da18eeff3
Nikita Zhuravets <impozernovus@gmail.com>
2023-05-18 21:23:33 +0300

    update to test

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import time
 counter = 0  # счетчик повторений
 
 while True:
-    counter += 1
+    counter = counter + 10 - 9
     print(f"Приложение работает. Повторение №{counter}")
     time.sleep(2)  # ожидание 3 секунды
